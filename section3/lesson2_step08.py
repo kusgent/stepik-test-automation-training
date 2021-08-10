02a3000d2208a15a1618c1484c23fe16309dc380
Kustov Ilya <kusgent@yandex.ru>
2021-08-10 23:06:53 +0300

    added solution lesson2_step08.py

---
+++ section3/lesson2_step08.py
@@ -0,0 +1,4 @@
+# https://stepik.org/lesson/36285/step/8?unit=162401
+
+def test_input_text(expected_result, actual_result):
+    assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
