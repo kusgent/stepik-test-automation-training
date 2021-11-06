6ba3fcb129dad7f4573ba1f1e3b0a587378f60e5
Kustov Ilya <kusgent@yandex.ru>
2021-11-06 20:58:44 +0300

    Add the code from lesson 6, step 7 (section 3)

---
+++ section3/test_rerun.py
@@ -0,0 +1,13 @@
+# https://stepik.org/lesson/237240/step/7?unit=209628
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#magic_link")
